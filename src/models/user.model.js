6392274b2c8bece55ef4a8f59549087d506d6224
Saral-Chauhangit <saralchauhan424t@gmail.com>
2025-04-21 17:35:36 +0530

    feat: add user and video models with authentication and pagination support
    - Added user model with fields for username, email, fullName, avatar, coverImage, watchHistory, password, and refreshToken.
    - Implemented password hashing and comparison using bcrypt in the user model.
    - Added video model with fields for videoFile, thumbNail, title, description, duration, views, isPublished, and owner.
    - Integrated mongoose-aggregate-paginate-v2 for pagination in the video model.
    - Updated package.json to include bcrypt, jsonwebtoken, and mongoose-aggregate-paginate-v2 as dependencies.

---
+++ src/models/user.model.js
@@ -0,0 +1,66 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import becrypt from "bcrypt";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      //cloudinary url
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      //cloudinary url
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Please enter a password"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save",async function (next) {
+    if (!this.isModified("password"))  return next();
+
+    this.password = await becrypt.hash(this.password, 10);
+    next();
+    
+});
+
+userSchema.methods.isPassordCorrect = async function (password) {
+  return await becrypt.compare(password, this.password);
+};
+
+export const User = mongoose.model("User", userSchema);
