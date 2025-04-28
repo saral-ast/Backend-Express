56a8cc052b4c7438ceadc98168a7b33d1d49df9a
Saral-Chauhangit <saralchauhan424t@gmail.com>
2025-04-28 16:12:53 +0530

    feat: enhance user model with token generation methods and add multer middleware for file uploads

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -6,7 +6,7 @@ class ApiError extends Error {
         stack = "",
     ){
         super(message);
-        this.statusCode = statusCode;
+        this.statusCode = statusCode; 
         this.message = message;
         this.data = null;
         this.success = false;
